b3fbcf6351ea958f470a4c30ed8c690769ec659d
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-02-23 02:27:48 -0600

    Revert "Delete 004_Comments.py"
    This reverts commit fbba87831388041b0236b836d3b53df92a6d5d36.

---
+++ 01_Python_Tutorial/004_Comments.py
@@ -0,0 +1,24 @@
+#Exammple 1
+#This is a comment
+print("Hello, World!")
+#Exammple 2
+print("Hello, World!") #This is a comment
+#Exammple 3
+#print("Hello, World!")
+print("Cheers, Mate!")
+#Exammple 4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+#Exammple 5
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+jls_extract_var = "Diego"
+print(jls_extract_var)
+
+
