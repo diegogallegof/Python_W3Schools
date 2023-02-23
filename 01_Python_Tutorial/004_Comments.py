f4c295dd42a0202f4c24faea522c9d83118cc9cf
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-02-23 02:00:25 -0600

    Create 004_Comments.py
    in this file we can find 5 examples related to Comments on Python

---
+++ 01_Python_Tutorial/004_Comments.py
@@ -0,0 +1,22 @@
+#Exammple 1
+#This is a comment
+print("Hello, World!")
+#Exammple 2
+print("Hello, World!") #This is a comment
+#Exammple 3
+#print("Hello, World!")
+print("Cheers, Mate!")
+#Exammple 4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+#Exammple 5
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+
