96ad4c48c6da4d767de4272e23d533c30a29dcb6
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-02-26 01:24:49 -0600

    Update 008_Ouput_variables.py

--- 01_Python_Tutorial/008_Ouput_variables.py
+++ 01_Python_Tutorial/008_Ouput_variables.py
@@ -47,7 +47,7 @@ y = "John"
 
 '''
 The best way to output multiple variables in the print() function is to separate them with commas, 
-which even support different data types:
+which even support different data types: 
 '''
 #Example
 x = 5
