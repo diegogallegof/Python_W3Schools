6a3ea9d4c36b703cf35aa91d2cea61c56bfe2582
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-02-26 01:00:58 -0600

    created variables_names

---
+++ 01_Python_Tutorial/006_Variables_names.py
@@ -0,0 +1,37 @@
+'''
+Variable Names
+A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). 
+Rules for Python variables:
+
+- A variable name must start with a letter or the underscore character
+- A variable name cannot start with a number
+- A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+- Variable names are case-sensitive (age, Age and AGE are three different variables)
+- A variable name cannot be any of the Python keywords.
+'''
+#Legal variable names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Illegal variable names:
+'''
+2myvar = "John"
+my-var = "John"
+my var = "John"
+'''
+
+#Camel Case
+#Each word, except the first, starts with a capital letter:
+myVariableName = "John"
+
+#Pascal Case
+#Each word starts with a capital letter:
+MyVariableName = "John"
+
+#Snake Case
+#Each word is separated by an underscore character:
+my_variable_name = "John"
