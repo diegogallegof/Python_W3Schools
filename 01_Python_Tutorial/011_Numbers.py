1a255bfd40d792672260874693359269e670d89f
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-02-28 23:55:00 -0600

    update numbers, create casting

--- 01_Python_Tutorial/011_Numbers.py
+++ 01_Python_Tutorial/011_Numbers.py
@@ -119,4 +119,6 @@ but Python has a built-in module called random that can be used to make random n
 #Example
 #Import the random module, and display a random number between 1 and 9:
 
+import random
+
 print(random.randrange(1, 10))
