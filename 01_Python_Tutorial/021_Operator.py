3fc2eede55561c0f51191a2f0097c2209a492ad8
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-04-20 23:08:04 -0600

    Create 021_Operator.py

---
+++ 01_Python_Tutorial/021_Operator.py
@@ -0,0 +1,51 @@
+# Python Operators
+# Operators are used to perform operations on variables and values.
+# In the example below, we use the + operator to add together two values:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+print(10 + 5)
+
+# Python divides the operators in the following groups:
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+# ----------------------------------------------------------------
+# Python Arithmetic Operators
+# ----------------------------------------------------------------
+# Arithmetic operators are used with numeric values 
+# to perform common mathematical operations:
+
+# Operator	Name	        Example
+# +	        Addition	    x + y	
+# -	        Subtraction	    x - y	
+# *	        Multiplication	x * y	
+# /	        Division	    x / y	
+# %	        Modulus	        x % y	
+# **	    Exponentiation	x ** y	
+# //	    Floor division	x // y
+# ----------------------------------------------------------------
+# Python Assignment Operators
+# ----------------------------------------------------------------
+# Assignment operators are used to assign values to variables:
+
+# Operator	Example	    Same As	
+# =	        x = 5	    x = 5	
+# +=	    x += 3	    x = x + 3	
+# -=	    x -= 3	    x = x - 3	
+# *=	    x *= 3	    x = x * 3	
+# /=	    x /= 3	    x = x / 3	
+# %=	    x %= 3	    x = x % 3	
+# //=	    x //= 3	    x = x // 3	
+# **=	    x **= 3	    x = x ** 3	
+# &=	    x &= 3	    x = x & 3	
+# |=	    x |= 3	    x = x | 3	
+# ^=	    x ^= 3	    x = x ^ 3	
+# >>=	    x >>= 3	    x = x >> 3	
+# <<=	    x <<= 3	    x = x << 3
+
