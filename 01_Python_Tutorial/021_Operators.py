d85907df241a65cce13b3c74e3bf7e1531e97ccb
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-04-21 23:42:28 -0600

    Update 021_Operators.py

--- 01_Python_Tutorial/021_Operators.py
+++ 01_Python_Tutorial/021_Operators.py
@@ -65,39 +65,46 @@ print(10 + 5)
 # ----------------------------------------------------------------
 # Logical operators are used to combine conditional statements:
 
-Operator	Description	Example	Try it
-and 	Returns True if both statements are true	x < 5 and  x < 10	
-or	Returns True if one of the statements is true	x < 5 or x < 4	
-not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+# Operator	Description	                                                Example
+# and 	    Returns True if both statements are true	                x < 5 and  x < 10	
+# or	    Returns True if one of the statements is true	            x < 5 or x < 4	
+# not	    Reverse the result, returns False if the result is true	    not(x < 5 and x < 10)
 # ----------------------------------------------------------------
 # Python Identity Operators
 # ----------------------------------------------------------------
 # Identity operators are used to compare the objects, not if they are equal, 
 # but if they are actually the same object, with the same memory location:
 
-Operator	Description	Example	Try it
-is 	Returns True if both variables are the same object	x is y	
-is not	Returns True if both variables are not the same object	x is not y	
+# Operator	Description	                                            Example	Try it
+# is 	    Returns True if both variables are the same object	    x is y	
+# is not	Returns True if both variables are not the same object	x is not y	
 # ----------------------------------------------------------------
 # Python Membership Operators
 # ----------------------------------------------------------------
 # Membership operators are used to test if a sequence is presented in an object:
 
-Operator	Description	Example	Try it
-in 	Returns True if a sequence with the specified value is present in the object	x in y	
-not in	Returns True if a sequence with the specified value is not present in the object	x not in y	
+# Operator	Description	                                                                        Example
+# in 	    Returns True if a sequence with the specified value is present in the object	    x in y	
+# not in	Returns True if a sequence with the specified value is not present in the object	x not in y	
 # ----------------------------------------------------------------
 # Python Bitwise Operators
 # ----------------------------------------------------------------
 # Bitwise operators are used to compare (binary) numbers:
 
-Operator	Name	Description	Example	Try it
-& 	AND	Sets each bit to 1 if both bits are 1	x & y	
-|	OR	Sets each bit to 1 if one of two bits is 1	x | y	
-^	XOR	Sets each bit to 1 if only one of two bits is 1	x ^ y	
-~	NOT	Inverts all the bits	~x	
-<<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off	x << 2	
->>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off	x >> 2	
+# Operator	Name	Description	                                        Example
+# & 	    AND	    Sets each bit to 1 if both bits are 1	            x & y	
+# |	        OR	    Sets each bit to 1 if one of two bits is 1	        x | y	
+# ^	        XOR	    Sets each bit to 1 if only one of two bits is 1	    x ^ y	
+# ~	        NOT	    Inverts all the bits	                            ~x	
+
+# <<	    Zero fill left shift	Shift left by pushing zeros 
+#                                   in from the right and let the 
+#                                   leftmost bits fall off	            x << 2	
+# >>	    Signed right shift	
+#                                   Shift right by pushing copies 
+#                                   of the leftmost bit in from the left, 
+#                                   and let the rightmost 
+#                                   bits fall off	                    x >> 2	
 # ----------------------------------------------------------------
 # Operator Precedence
 # ----------------------------------------------------------------
