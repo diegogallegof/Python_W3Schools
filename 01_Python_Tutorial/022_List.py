719b7feb09a6e7d4f701553a30b22cebbf2f2555
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-04-23 23:34:34 -0600

    Create 022_List.py

---
+++ 01_Python_Tutorial/022_List.py
@@ -0,0 +1,17 @@
+# Python Lists
+mylist = ["apple", "banana", "cherry"]
+
+# Lists are used to store multiple items in a single variable.
+# Lists are one of 4 built-in data types in Python used to 
+# store collections of data, the other 3 are Tuple, Set, 
+# and Dictionary, all with different qualities and usage.
+
+# Lists are created using square brackets:
+
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Create a List:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
