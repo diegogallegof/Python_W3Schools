ffe7e03732d71c9715f7ce459255b77033f0454d
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-04-25 23:17:34 -0600

    Update 023_Access_list_items.py

--- 01_Python_Tutorial/023_Access_list_items.py
+++ 01_Python_Tutorial/023_Access_list_items.py
@@ -7,3 +7,16 @@
 
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1])
+# Note: The first item has index 0.
+
+# Negative Indexing
+# Negative indexing means start from the end
+
+# -1 refers to the last item, -2 refers to the second last item etc.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Print the last item of the list:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
