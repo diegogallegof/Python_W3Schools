05ea9236c5ef7ef58fd9d832e0ee1b9c749b4ffe
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-04-25 23:12:29 -0600

    Create 023_Access_list_items.py

---
+++ 01_Python_Tutorial/023_Access_list_items.py
@@ -0,0 +1,9 @@
+# Access Items
+# List items are indexed and you can access them by referring to the index number:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Print the second item of the list:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
