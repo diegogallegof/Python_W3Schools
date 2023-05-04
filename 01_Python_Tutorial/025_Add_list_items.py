b5747813a85ac4d82dcc4fb90a0ae59c476bfed3
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-05-03 23:12:14 -0600

    Update 025_Add_list_items.py

--- 01_Python_Tutorial/025_Add_list_items.py
+++ 01_Python_Tutorial/025_Add_list_items.py
@@ -33,3 +33,18 @@ thislist = ["apple", "banana", "cherry"]
 tropical = ["mango", "pineapple", "papaya"]
 thislist.extend(tropical)
 print(thislist)
+# The elements will be added to the end of the list.
+
+# Add Any Iterable
+# The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
+# ---------------------------------------------------------------
+# Example
+# ---------------------------------------------------------------
+# Add elements of a tuple to a list:
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+a = "abcdefghijklmnñopqrstuvwxyz"
+thislist.extend(thistuple)
+thislist.extend(a)
+print(thislist)
