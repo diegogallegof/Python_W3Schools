ec374eb480bf474bb4d75a5706ddfb78a20961a4
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-05-05 23:22:42 -0600

    Update 026_Remove_list_items.py

--- 01_Python_Tutorial/026_Remove_list_items.py
+++ 01_Python_Tutorial/026_Remove_list_items.py
@@ -18,3 +18,13 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 thislist.pop(1)
 print(thislist)
+
+# If you do not specify the index, the pop() method removes the last item.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Remove the last item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
