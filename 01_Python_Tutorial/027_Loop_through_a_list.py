f6c24fa134e5766b74c924ffa5ff1af5de8fe40b
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-05-09 22:44:40 -0600

    Create 027_Loop_through_a_list.py

---
+++ 01_Python_Tutorial/027_Loop_through_a_list.py
@@ -0,0 +1,10 @@
+# Loop Through a List
+# You can loop through the list items by using a for loop:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Print all items in the list, one by one:
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
