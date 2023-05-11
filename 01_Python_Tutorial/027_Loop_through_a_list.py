0aaaa0df7ebfdeff7db55d73fcae65698cf0a074
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-05-10 23:41:27 -0600

    Update 027_Loop_through_a_list.py

--- 01_Python_Tutorial/027_Loop_through_a_list.py
+++ 01_Python_Tutorial/027_Loop_through_a_list.py
@@ -8,3 +8,16 @@
 thislist = ["apple", "banana", "cherry"]
 for x in thislist:
   print(x)
+
+#   Loop Through the Index Numbers
+# You can also loop through the list items by referring to their index number.
+
+# Use the range() and len() functions to create a suitable iterable.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Print all items by referring to their index number:
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
