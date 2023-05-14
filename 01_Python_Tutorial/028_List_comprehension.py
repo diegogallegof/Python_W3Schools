f94849f92f215e6ac9f2749e7eae54f4c2bb0ad9
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-05-14 09:20:49 -0600

    Update 028_List_comprehension.py

--- 01_Python_Tutorial/028_List_comprehension.py
+++ 01_Python_Tutorial/028_List_comprehension.py
@@ -17,3 +17,13 @@ for x in fruits:
     newlist.append(x)
 
 print(newlist)
+
+# With list comprehension you can do all that with only one line of code:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
