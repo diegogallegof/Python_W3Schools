9f4176985b732fdf97e0b1136411b39719ea25c1
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-05-16 23:33:42 -0600

    Update 028_List_comprehension.py

--- 01_Python_Tutorial/028_List_comprehension.py
+++ 01_Python_Tutorial/028_List_comprehension.py
@@ -40,3 +40,13 @@ print(newlist)
 # Only accept items that are not "apple":
 
 newlist = [x for x in fruits if x != "apple"]
+# The condition if x != "apple"  will return True for all elements other than "apple", making the new list contain all fruits except "apple".
+
+# The condition is optional and can be omitted:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# With no if statement:
+
+newlist = [x for x in fruits]
+print(newlist)
