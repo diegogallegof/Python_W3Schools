f13c3185b1155dca1d9388ea3269ee96ad8f518c
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-05-24 23:41:54 -0600

    Create 029_Sort_list.py

---
+++ 01_Python_Tutorial/029_Sort_list.py
@@ -0,0 +1,10 @@
+# Sort List Alphanumerically
+# List objects have a sort() method that will sort the list alphanumerically, ascending, by default:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Sort the list alphabetically:
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
