722dff85f19032d7c23078ba3596cd7bd461d0eb
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-05-28 21:27:53 -0600

    Update 029_Sort_list.py

--- 01_Python_Tutorial/029_Sort_list.py
+++ 01_Python_Tutorial/029_Sort_list.py
@@ -26,3 +26,11 @@ print(thislist)
 thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
 thislist.sort(reverse = True)
 print(thislist)
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Sort the list descending:
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
