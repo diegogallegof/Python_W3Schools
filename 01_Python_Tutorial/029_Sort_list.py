8ee41bb060d4650361becfb12a4a733d12944475
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-05-29 22:41:39 -0600

    Update 029_Sort_list.py

--- 01_Python_Tutorial/029_Sort_list.py
+++ 01_Python_Tutorial/029_Sort_list.py
@@ -34,3 +34,19 @@ print(thislist)
 thislist = [100, 50, 65, 82, 23]
 thislist.sort(reverse = True)
 print(thislist)
+
+# Customize Sort Function
+# You can also customize your own function by using the keyword argument key = function.
+
+# The function will return a number that will be used to sort the list (the lowest number first):
+# ------------------------------------
+# Example
+# ------------------------------------
+# Sort the list based on how close the number is to 50:
+
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
