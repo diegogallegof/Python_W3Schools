b6d361c57f883c65537df039234c341528c16bc1
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-05-31 23:23:07 -0600

    Update 029_Sort_list.py

--- 01_Python_Tutorial/029_Sort_list.py
+++ 01_Python_Tutorial/029_Sort_list.py
@@ -61,3 +61,14 @@ print(thislist)
 thislist = ["banana", "Orange", "Kiwi", "cherry"]
 thislist.sort()
 print(thislist)
+# Luckily we can use built-in functions as key functions when sorting a list.
+
+# So if you want a case-insensitive sort function, use str.lower as a key function:
+# ------------------------------------
+# Example
+# ------------------------------------
+# Perform a case-insensitive sort of the list:
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
