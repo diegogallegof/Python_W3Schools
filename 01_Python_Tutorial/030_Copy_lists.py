b2b464b8b318e5857055f9a63e4008ecf8d81d27
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-06-03 00:20:54 -0600

    Update 030_Copy_lists.py

--- 01_Python_Tutorial/030_Copy_lists.py
+++ 01_Python_Tutorial/030_Copy_lists.py
@@ -10,3 +10,13 @@
 thislist = ["apple", "banana", "cherry"]
 mylist = thislist.copy()
 print(mylist)
+
+# Another way to make a copy is to use the built-in method list().
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Make a copy of a list with the list() method:
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
