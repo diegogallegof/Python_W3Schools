c5d5bc512e9a19e1a69d466d60065ab37a6b683d
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-06-10 13:35:56 -0600

    Update 033_Tuples.py

--- 01_Python_Tutorial/033_Tuples.py
+++ 01_Python_Tutorial/033_Tuples.py
@@ -41,7 +41,7 @@ print(thistuple)
 # To determine how many items a tuple has, use the len() function:
 # -------------------------
 # Example
-# ----------------------------
+# ----------------------------------
 # Print the number of items in the tuple:
 
 thistuple = ("apple", "banana", "cherry")
