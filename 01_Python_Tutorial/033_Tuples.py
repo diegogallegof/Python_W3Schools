6a964917bd0221e442f50ac89189ae230fb61b22
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-06-10 13:27:05 -0600

    Create 033_Tuples.py

---
+++ 01_Python_Tutorial/033_Tuples.py
@@ -0,0 +1,17 @@
+# Python Tuples
+# mytuple = ("apple", "banana", "cherry")
+# Tuple
+# Tuples are used to store multiple items in a single variable.
+
+# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+
+# A tuple is a collection which is ordered and unchangeable.
+
+# Tuples are written with round brackets.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Create a Tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
