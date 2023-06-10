ae4423d53c94f7089249522ab3a13589f3463209
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-06-10 13:33:15 -0600

    List and Tuples

--- 01_Python_Tutorial/033_Tuples.py
+++ 01_Python_Tutorial/033_Tuples.py
@@ -15,3 +15,34 @@
 
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
+
+# Tuple Items
+# Tuple items are ordered, unchangeable, and allow duplicate values.
+
+# Tuple items are indexed, the first item has index [0], the second item has index [1] etc.
+
+# Ordered
+# When we say that tuples are ordered, it means that the items have a defined order, and that order will not change.
+
+# Unchangeable
+# Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
+
+# Allow Duplicates
+# Since tuples are indexed, they can have items with the same value:
+# -------------------------------------
+# Example
+# -------------------------------------
+# Tuples allow duplicate values:
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+# Tuple Length
+# To determine how many items a tuple has, use the len() function:
+# -------------------------
+# Example
+# -------------------------
+# Print the number of items in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
