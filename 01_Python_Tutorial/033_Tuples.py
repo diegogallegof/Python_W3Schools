60cf549ff62452f3f99c29843cd17aafe4c9bbf9
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-06-12 23:34:33 -0600

    Update 033_Tuples.py

--- 01_Python_Tutorial/033_Tuples.py
+++ 01_Python_Tutorial/033_Tuples.py
@@ -70,3 +70,10 @@ print(type(thistuple))
 tuple1 = ("apple", "banana", "cherry")
 tuple2 = (1, 5, 7, 9, 3)
 tuple3 = (True, False, False)
+# A tuple can contain different data types:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# A tuple with strings, integers and boolean values:
+
+tuple1 = ("abc", 34, True, 40, "male")
