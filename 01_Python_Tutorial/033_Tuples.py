d9a665d0e6f06168817f3b5fc0665df1bad0707c
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-06-14 22:41:09 -0600

    Update 033_Tuples.py

--- 01_Python_Tutorial/033_Tuples.py
+++ 01_Python_Tutorial/033_Tuples.py
@@ -88,3 +88,13 @@ tuple1 = ("abc", 34, True, 40, "male")
 
 mytuple = ("apple", "banana", "cherry")
 print(type(mytuple))
+
+# The tuple() Constructor
+# It is also possible to use the tuple() constructor to make a tuple.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Using the tuple() method to make a tuple:
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
