546f4b6401b3852fc7bb051468d7a3e29ce08e0d
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-06-15 23:43:05 -0600

    Update 033_Tuples.py

--- 01_Python_Tutorial/033_Tuples.py
+++ 01_Python_Tutorial/033_Tuples.py
@@ -98,3 +98,16 @@ print(type(mytuple))
 
 thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
 print(thistuple)
+
+# Python Collections (Arrays)
+# There are four collection data types in the Python programming language:
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+# *Set items are unchangeable, but you can remove and/or add items whenever you like.
+
+# **As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
+
+# When choosing a collection type, it is useful to understand the properties of that type. Choosing the right type for a particular data set could mean retention of meaning, and, it could mean an increase in efficiency or security.
