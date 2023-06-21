a8aa6e07947d218a83d712de4e80885cd4c2037b
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-06-20 23:47:06 -0600

    Update 034_Access_tuple_items.py

--- 01_Python_Tutorial/034_Access_tuple_items.py
+++ 01_Python_Tutorial/034_Access_tuple_items.py
@@ -10,3 +10,14 @@ print(thistuple[1])
 # Note: The first item has index 0.
 #
 ##
+# Negative Indexing
+# Negative indexing means start from the end.
+
+# -1 refers to the last item, -2 refers to the second last item etc.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Print the last item of the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
