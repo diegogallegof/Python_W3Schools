ad9b8898fedfa6c3b885e515e4b0f962be867850
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-06-26 23:59:05 -0600

    Update 034_Access_tuple_items.py

--- 01_Python_Tutorial/034_Access_tuple_items.py
+++ 01_Python_Tutorial/034_Access_tuple_items.py
@@ -54,3 +54,11 @@ print(thistuple[:4])
 
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[2:])
+# Range of Negative Indexes
+# Specify negative indexes if you want to start the search from the end of the tuple:
+
+# Example
+# This example returns the items from index -4 (included) to index -1 (excluded)
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
