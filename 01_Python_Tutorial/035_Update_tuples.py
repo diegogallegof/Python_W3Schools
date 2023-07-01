dadf2a9a1f235defeb846aa73b520cadda2ba895
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-06-30 23:54:25 -0600

    Update 035_Update_tuples.py

--- 01_Python_Tutorial/035_Update_tuples.py
+++ 01_Python_Tutorial/035_Update_tuples.py
@@ -32,3 +32,15 @@ thistuple = ("apple", "banana", "cherry")
 y = list(thistuple)
 y.append("orange")
 thistuple = tuple(y)
+
+# 2. Add tuple to a tuple. You are allowed to add tuples to tuples, so if you want to add one item, (or many), create a new tuple with the item(s), and add it to the existing tuple:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Create a new tuple with the value "orange", and add that tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
