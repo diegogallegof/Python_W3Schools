5f6073d7b9214e2bf60522e6c3b1995d15d6f2bc
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-07-03 23:59:37 -0600

    Update 037_Loop_through_a_tuple.py

--- 01_Python_Tutorial/037_Loop_through_a_tuple.py
+++ 01_Python_Tutorial/037_Loop_through_a_tuple.py
@@ -7,3 +7,18 @@
 thistuple = ("apple", "banana", "cherry")
 for x in thistuple:
   print(x)
+
+# Learn more about for loops in our Python For Loops Chapter.
+
+# Loop Through the Index Numbers
+# You can also loop through the tuple items by referring to their index number.
+
+# Use the range() and len() functions to create a suitable iterable.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Print all items by referring to their index number:
+
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
