9e538785849a1f0f5dff77a77dbfe61e0239047f
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-07-03 23:51:30 -0600

    Create 036_Unpackin_a_tuple.py

---
+++ 01_Python_Tutorial/036_Unpackin_a_tuple.py
@@ -0,0 +1,8 @@
+# Unpacking a Tuple
+# When we create a tuple, we normally assign values to it. This is called "packing" a tuple:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Packing a tuple:
+
+fruits = ("apple", "banana", "cherry")
