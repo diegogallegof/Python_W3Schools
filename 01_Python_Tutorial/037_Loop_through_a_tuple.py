b3c1fb58e04778c84fabd7915905d22be421d0b1
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-07-03 23:58:34 -0600

    Create 037_Loop_through_a_tuple.py

---
+++ 01_Python_Tutorial/037_Loop_through_a_tuple.py
@@ -0,0 +1,9 @@
+# You can loop through the tuple items by using a for loop.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Iterate through the items and print the values:
+
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
