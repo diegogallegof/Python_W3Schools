584fd17c61f06aed7913a39996d5c14849607c7d
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-07-12 23:56:34 -0600

    Update 040_Python_sets.py

--- 01_Python_Tutorial/040_Python_sets.py
+++ 01_Python_Tutorial/040_Python_sets.py
@@ -17,3 +17,29 @@ myset = {"apple", "banana", "cherry"}
 
 thisset = {"apple", "banana", "cherry"}
 print(thisset)
+
+# Note: Sets are unordered, so you cannot be sure in which order the items will appear.
+
+# Set Items
+# Set items are unordered, unchangeable, and do not allow duplicate values.
+
+# Unordered
+# Unordered means that the items in a set do not have a defined order.
+
+# Set items can appear in a different order every time you use them, and cannot be referred to by index or key.
+
+# Unchangeable
+# Set items are unchangeable, meaning that we cannot change the items after the set has been created.
+
+# Once a set is created, you cannot change its items, but you can remove items and add new items.
+
+# Duplicates Not Allowed
+# Sets cannot have two items with the same value.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Duplicate values will be ignored:
+
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
