9335fdc3aca972c8b70d5028fa2fc366593c5ccb
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-07-27 23:40:03 -0600

    Update 040_Python_sets.py

--- 01_Python_Tutorial/040_Python_sets.py
+++ 01_Python_Tutorial/040_Python_sets.py
@@ -63,3 +63,14 @@ print(thisset1)
 thisset = {"apple", "banana", "cherry"}
 
 print(len(thisset))
+
+# Set Items - Data Types
+# Set items can be of any data type:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# String, int and boolean data types:
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
