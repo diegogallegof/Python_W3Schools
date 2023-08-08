4ef8e2ab646464094a1c74f4f0846aafd0d564eb
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-08-07 23:49:47 -0600

    Create 041_Access_set_items.py

---
+++ 01_Python_Tutorial/041_Access_set_items.py
@@ -0,0 +1,13 @@
+# Access Items
+# You cannot access items in a set by referring to an index or a key.
+
+# But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Loop through the set, and print the values:
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
