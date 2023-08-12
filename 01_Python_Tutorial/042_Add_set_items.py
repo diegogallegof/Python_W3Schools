87c595148e5418ef7fe6838c8c5f1ee52f7b1fdb
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-08-11 23:53:02 -0600

    Create 042_Add_set_items.py

---
+++ 01_Python_Tutorial/042_Add_set_items.py
@@ -0,0 +1,14 @@
+# Add Items
+# Once a set is created, you cannot change its items, but you can add new items.
+
+# To add one item to a set use the add() method.
+# ----------------------------------------------------------------
+# Example
+#----------------------------------------------------------------
+# Add an item to a set, using the add() method:
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
