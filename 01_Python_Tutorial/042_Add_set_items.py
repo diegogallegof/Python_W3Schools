321b29dbff3b534c464b2d1de90401d6d24ae5ca
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-08-16 00:16:22 -0600

    Update 042_Add_set_items.py

--- 01_Python_Tutorial/042_Add_set_items.py
+++ 01_Python_Tutorial/042_Add_set_items.py
@@ -26,3 +26,17 @@ tropical = {"pineapple", "mango", "papaya"}
 thisset.update(tropical)
 
 print(thisset)
+
+# Add Any Iterable
+# The object in the update() method does not have to be a set, it can be any iterable object (tuples, lists, dictionaries etc.).
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Add elements of a list to at set:
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
