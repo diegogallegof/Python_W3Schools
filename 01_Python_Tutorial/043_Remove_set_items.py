e715a866e1138cb5e4a790f832e5693588089496
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-08-16 23:57:54 -0600

    Create 043_Remove_set_items.py

---
+++ 01_Python_Tutorial/043_Remove_set_items.py
@@ -0,0 +1,11 @@
+# Remove Item
+# To remove an item in a set, use the remove(), or the discard() method.
+
+# ExampleGet your own Python Server
+# Remove "banana" by using the remove() method:
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
