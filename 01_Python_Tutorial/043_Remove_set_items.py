d326383b8802cea9aaf8a97ab06f002a353ad9e4
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-08-17 23:01:47 -0600

    Update 043_Remove_set_items.py

--- 01_Python_Tutorial/043_Remove_set_items.py
+++ 01_Python_Tutorial/043_Remove_set_items.py
@@ -9,3 +9,14 @@ thisset = {"apple", "banana", "cherry"}
 thisset.remove("banana")
 
 print(thisset)
+
+# Note: If the item to remove does not exist, remove() will raise an error.
+
+# Example
+# Remove "banana" by using the discard() method:
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
