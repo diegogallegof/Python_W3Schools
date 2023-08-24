7bda1075cbd232d0f079240c0f7fb834617f04f1
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-08-23 23:58:18 -0600

    Create 044_Loop_sets.py

---
+++ 01_Python_Tutorial/044_Loop_sets.py
@@ -0,0 +1,11 @@
+# Loop Items
+# You can loop through the set items by using a for loop:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Loop through the set, and print the values:
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
