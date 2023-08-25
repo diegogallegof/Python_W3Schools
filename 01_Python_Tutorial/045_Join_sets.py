e2b69bee6c36cd7c35c31386f18d412ddf664569
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-08-24 23:43:52 -0600

    Update 045_Join_sets.py

--- 01_Python_Tutorial/045_Join_sets.py
+++ 01_Python_Tutorial/045_Join_sets.py
@@ -11,3 +11,13 @@ set2 = {1, 2, 3}
 
 set3 = set1.union(set2)
 print(set3)
+# --------------------------------------------------------------------
+# Example
+# --------------------------------------------------------------------
+# The update() method inserts the items in set2 into set1:
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set1.update(set2)
+print(set1)
