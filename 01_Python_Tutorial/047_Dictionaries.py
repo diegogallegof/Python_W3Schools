9d8bc0e2d8d08f010c59973855b7f0e3e36ad5e9
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-09-06 23:50:35 -0600

    Update 047_Dictionaries.py

--- 01_Python_Tutorial/047_Dictionaries.py
+++ 01_Python_Tutorial/047_Dictionaries.py
@@ -24,3 +24,19 @@ thisdict = {
   "year": 1964
 }
 print(thisdict)
+
+# Dictionary Items
+# Dictionary items are ordered, changeable, and does not allow duplicates.
+
+# Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+# ------------------------------------------
+# Example
+# ------------------------------------------
+# Print the "brand" value of the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
