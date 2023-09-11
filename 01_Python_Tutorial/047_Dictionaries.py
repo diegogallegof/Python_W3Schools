2d6038fac96365f77d7ea9284684f0217e28feac
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-09-10 23:16:12 -0600

    Update 047_Dictionaries.py

--- 01_Python_Tutorial/047_Dictionaries.py
+++ 01_Python_Tutorial/047_Dictionaries.py
@@ -74,3 +74,17 @@ print(thisdict)
 # Print the number of items in the dictionary:
 
 print(len(thisdict))
+
+# Dictionary Items - Data Types
+# The values in dictionary items can be of any data type:
+# ----------------------------------------------------------------
+# Example
+#  ----------------------------------------------------------------
+# String, int, boolean, and list data types:
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
