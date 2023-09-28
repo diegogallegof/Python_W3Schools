0cbcfea51c36e94bd853c8dda42c7934d02835dc
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-09-27 23:54:07 -0500

    Create 048_Access Dictionary Items.py

---
+++ 01_Python_Tutorial/048_Access Dictionary Items.py
@@ -0,0 +1,12 @@
+# Accessing Items
+# You can access the items of a dictionary by referring to its key name, inside square brackets:
+
+# Example
+# Get the value of the "model" key:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
