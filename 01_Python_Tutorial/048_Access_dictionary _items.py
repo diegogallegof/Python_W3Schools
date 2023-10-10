b7cd2e14584dad16adf328c3e1b6442f50027cbe
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-10-09 23:18:40 -0600

    Update 048_Access_dictionary _items.py

--- 01_Python_Tutorial/048_Access_dictionary _items.py
+++ 01_Python_Tutorial/048_Access_dictionary _items.py
@@ -27,3 +27,23 @@ x = thisdict.get("model")
 # Get a list of the keys:
 
 x = thisdict.keys()
+
+# The list of the keys is a view of the dictionary, meaning that any changes done to the dictionary will be reflected in the keys list.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Add a new item to the original dictionary, and see that the keys list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
