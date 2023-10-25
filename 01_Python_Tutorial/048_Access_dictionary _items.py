53bf4e4a5ad240349d7f918a56189c552a62dc69
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-10-24 23:50:03 -0600

    Update 048_Access_dictionary _items.py

--- 01_Python_Tutorial/048_Access_dictionary _items.py
+++ 01_Python_Tutorial/048_Access_dictionary _items.py
@@ -150,3 +150,23 @@ print(x) #after the change
 # Get a list of the key:value pairs
 
 x = thisdict.items()
+
+# The returned list is a view of the items of the dictionary, meaning that any changes done to the dictionary will be reflected in the items list.
+# ---------------------------
+# Example
+# ----------------------------
+# Make a change in the original dictionary, and see that the items list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.items()
+
+print(x) #before the change
+
+car["year"] = 2020
+
+print(x) #after the change
