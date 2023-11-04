a45da9b67d3dac86d601423a2130c7bb35f21234
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-11-03 18:32:05 -0600

    Create 049_Change_dictionary_items.py

---
+++ 01_Python_Tutorial/049_Change_dictionary_items.py
@@ -0,0 +1,13 @@
+# Change Values
+# You can change the value of a specific item by referring to its key name:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Change the "year" to 2018:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
