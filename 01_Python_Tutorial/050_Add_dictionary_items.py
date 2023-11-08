4af37d2e0fa8f36a162bbac534438e41d60208fc
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-11-07 23:55:38 -0600

    Update 050_Add_dictionary_items.py

--- 01_Python_Tutorial/050_Add_dictionary_items.py
+++ 01_Python_Tutorial/050_Add_dictionary_items.py
@@ -10,3 +10,19 @@ thisdict = {
 }
 thisdict["color"] = "red"
 print(thisdict)
+
+# Update Dictionary
+# The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
+
+# The argument must be a dictionary, or an iterable object with key:value pairs.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Add a color item to the dictionary by using the update() method:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
