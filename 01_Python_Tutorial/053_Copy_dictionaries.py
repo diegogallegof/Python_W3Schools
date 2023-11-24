45c8bdd142c6247d56a07c052bce73a4be7351f3
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-11-23 23:58:46 -0600

    Update 053_Copy_dictionaries.py

--- 01_Python_Tutorial/053_Copy_dictionaries.py
+++ 01_Python_Tutorial/053_Copy_dictionaries.py
@@ -14,3 +14,16 @@ thisdict = {
 }
 mydict = thisdict.copy()
 print(mydict)
+
+Another way to make a copy is to use the built-in function dict().
+
+Example
+Make a copy of a dictionary with the dict() function:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
