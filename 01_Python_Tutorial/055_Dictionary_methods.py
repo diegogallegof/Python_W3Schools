8353a15c9c353583547229cc56f97211860fa2c4
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-11-28 23:53:54 -0600

    Create 055_Dictionary_methods.py

---
+++ 01_Python_Tutorial/055_Dictionary_methods.py
@@ -0,0 +1,15 @@
+# Dictionary Methods
+# Python has a set of built-in methods that you can use on dictionaries.
+
+# Method	Description
+# clear()	Removes all the elements from the dictionary
+# copy()	Returns a copy of the dictionary
+# fromkeys()	Returns a dictionary with the specified keys and value
+# get()	Returns the value of the specified key
+# items()	Returns a list containing a tuple for each key value pair
+# keys()	Returns a list containing the dictionary's keys
+# pop()	Removes the element with the specified key
+# popitem()	Removes the last inserted key-value pair
+# setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+# update()	Updates the dictionary with the specified key-value pairs
+# values()	Returns a list of all the values in the dictionary
