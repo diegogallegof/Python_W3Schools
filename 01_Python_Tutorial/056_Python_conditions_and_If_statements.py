3ac4f9f782730f9c9257ce0bdeca8d2d5d214673
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-02 23:17:09 -0600

    Update 056_Python_conditions_and_If_statements.py

--- 01_Python_Tutorial/056_Python_conditions_and_If_statements.py
+++ 01_Python_Tutorial/056_Python_conditions_and_If_statements.py
@@ -56,3 +56,15 @@ b = 200
 if b > a:
 print("b is greater than a") # you will get an error
 
+# Elif
+# The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
