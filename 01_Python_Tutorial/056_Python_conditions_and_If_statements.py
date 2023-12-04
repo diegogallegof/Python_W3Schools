c4acdc77d0b17a9797bb0215b20b4529f9db20a8
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-04 00:03:01 -0600

    Update 056_Python_conditions_and_If_statements.py

--- 01_Python_Tutorial/056_Python_conditions_and_If_statements.py
+++ 01_Python_Tutorial/056_Python_conditions_and_If_statements.py
@@ -84,3 +84,16 @@ elif a == b:
 else:
   print("a is greater than b")
 
+# In this example a is greater than b, so the first condition is not true, also the elif condition is not true, so we go to the else condition and print to screen that "a is greater than b".
+
+# You can also have an else without the elif:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
