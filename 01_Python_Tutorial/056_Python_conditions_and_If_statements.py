1d847eb8388fef897ceb9b113292d80283657c6f
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-03 23:51:30 -0600

    Update 056_Python_conditions_and_If_statements.py

--- 01_Python_Tutorial/056_Python_conditions_and_If_statements.py
+++ 01_Python_Tutorial/056_Python_conditions_and_If_statements.py
@@ -68,3 +68,19 @@ if b > a:
 elif a == b:
   print("a and b are equal")
 
+# In this example a is equal to b, so the first condition is not true, but the elif condition is true, so we print to screen that "a and b are equal".
+
+# Else
+# The else keyword catches anything which isn't caught by the preceding conditions.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
