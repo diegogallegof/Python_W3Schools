1e3fc0d4e5f21f75f6f2aa0668021aff84880e43
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-07 01:27:21 -0600

    Update 056_Python_conditions_and_If_statements.py

--- 01_Python_Tutorial/056_Python_conditions_and_If_statements.py
+++ 01_Python_Tutorial/056_Python_conditions_and_If_statements.py
@@ -128,3 +128,16 @@ print("A") if a > b else print("B")
 a = 330
 b = 330
 print("A") if a > b else print("=") if a == b else print("B")
+
+# And
+# The and keyword is a logical operator, and is used to combine conditional statements:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Test if a is greater than b, AND if c is greater than a:
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
