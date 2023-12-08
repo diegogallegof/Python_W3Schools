ef84b18daf7456dd40cbc40f0eb3d18ca5ff246d
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-07 23:48:31 -0600

    Update 056_Python_conditions_and_If_statements.py

--- 01_Python_Tutorial/056_Python_conditions_and_If_statements.py
+++ 01_Python_Tutorial/056_Python_conditions_and_If_statements.py
@@ -141,3 +141,16 @@ b = 33
 c = 500
 if a > b and c > a:
   print("Both conditions are True")
+
+# Or
+# The or keyword is a logical operator, and is used to combine conditional statements:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Test if a is greater than b, OR if a is greater than c:
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
