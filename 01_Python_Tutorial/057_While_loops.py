392e69a31282264597ed8aa1e06a160d21f70f42
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-11 23:57:40 -0600

    Create 057_While_loops.py

---
+++ 01_Python_Tutorial/057_While_loops.py
@@ -0,0 +1,16 @@
+# Python Loops
+# Python has two primitive loop commands:
+
+# while loops
+# for loops
+# The while Loop
+# With the while loop we can execute a set of statements as long as a condition is true.
+# --------------------------------------------------------------------
+# Example
+# --------------------------------------------------------------------
+Print i as long as i is less than 6:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
