0e3a280fb60027ea4af74dd35bd1f1433a38fccc
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-13 00:02:10 -0600

    Update 057_While_loops.py

--- 01_Python_Tutorial/057_While_loops.py
+++ 01_Python_Tutorial/057_While_loops.py
@@ -32,3 +32,17 @@ while i < 6:
   if i == 3:
     break
   i += 1
+
+# The continue Statement
+# With the continue statement we can stop the current iteration, and continue with the next:
+# --------------------------------------------------------------------
+# Example
+# --------------------------------------------------------------------
+# Continue to the next iteration if i is 3:
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
