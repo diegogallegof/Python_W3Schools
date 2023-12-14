13a6c12726a18b51c525b6d32177cb0da2c41fdb
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-14 01:52:16 -0600

    Update 057_While_loops.py

--- 01_Python_Tutorial/057_While_loops.py
+++ 01_Python_Tutorial/057_While_loops.py
@@ -46,3 +46,17 @@ while i < 6:
   if i == 3:
     continue
   print(i)
+
+# The else Statement
+# With the else statement we can run a block of code once when the condition no longer is true:
+# --------------------------------------------------------------------
+# Example
+# --------------------------------------------------------------------
+# Print a message once the condition is false:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
