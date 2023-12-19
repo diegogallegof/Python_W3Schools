0aaa2787c4736e62a9bea248fd7ca4804e967e9a
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-19 00:03:53 -0600

    Update 058_For_Loops.py

--- 01_Python_Tutorial/058_For_Loops.py
+++ 01_Python_Tutorial/058_For_Loops.py
@@ -46,3 +46,16 @@ for x in fruits:
   if x == "banana":
     break
   print(x)
+
+# The continue Statement
+# With the continue statement we can stop the current iteration of the loop, and continue with the next:
+# -------------------------------------------------------------
+# Example
+# -------------------------------------------------------------
+# Do not print banana:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
