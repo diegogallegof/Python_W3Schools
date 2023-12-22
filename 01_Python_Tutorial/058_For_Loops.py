9524501d5fa94ba97c498bb283e8a677941dd237
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-21 23:53:09 -0600

    Update 058_For_Loops.py

--- 01_Python_Tutorial/058_For_Loops.py
+++ 01_Python_Tutorial/058_For_Loops.py
@@ -81,3 +81,12 @@ for x in range(6):
 
 for x in range(2, 6):
   print(x)
+
+# The range() function defaults to increment the sequence by 1, however it is possible to specify the increment value by adding a third parameter: range(2, 30, 3):
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Increment the sequence with 3 (default is 1):
+
+for x in range(2, 30, 3):
+  print(x)
