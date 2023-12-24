5039261884012a77f324d3cd9e61909ffe694192
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-24 17:13:28 -0600

    Update 058_For_Loops.py

--- 01_Python_Tutorial/058_For_Loops.py
+++ 01_Python_Tutorial/058_For_Loops.py
@@ -114,3 +114,19 @@ for x in range(6):
   print(x)
 else:
   print("Finally finished!")
+
+# Nested Loops
+# A nested loop is a loop inside a loop.
+
+# The "inner loop" will be executed one time for each iteration of the "outer loop":
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Print each adjective for every fruit:
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
