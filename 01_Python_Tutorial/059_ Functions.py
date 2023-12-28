d83a8a56d63f882459dcee1ed186e1a2af4fb3f2
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-27 22:25:45 -0600

    Update 059_ Functions.py

--- 01_Python_Tutorial/059_ Functions.py
+++ 01_Python_Tutorial/059_ Functions.py
@@ -12,3 +12,13 @@
 # ----------------------------------------------------------------
 def my_function():
   print("Hello from a function")
+
+# Calling a Function
+# To call a function, use the function name followed by parenthesis:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+def my_function():
+  print("Hello from a function")
+
+my_function()
