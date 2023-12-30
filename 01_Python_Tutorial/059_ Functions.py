5ea57db2e6befa270babc1ac00569fc28c804689
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2023-12-29 22:52:31 -0600

    Update 059_ Functions.py

--- 01_Python_Tutorial/059_ Functions.py
+++ 01_Python_Tutorial/059_ Functions.py
@@ -38,3 +38,25 @@ def my_function(fname):
 my_function("Emil")
 my_function("Tobias")
 my_function("Linus")
+
+# Arguments are often shortened to args in Python documentations.
+# Parameters or Arguments?
+# The terms parameter and argument can be used for the same thing: information that are passed into a function.
+
+# From a function's perspective:
+
+# A parameter is the variable listed inside the parentheses in the function definition.
+
+# An argument is the value that is sent to the function when it is called.
+
+# Number of Arguments
+# By default, a function must be called with the correct number of arguments. Meaning that if your function expects 2 arguments, you have to call the function with 2 arguments, not more, and not less.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# This function expects 2 arguments, and gets 2 arguments:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
