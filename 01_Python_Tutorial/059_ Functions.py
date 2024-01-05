56e294efe2ab0fad606f3bfad078895feace878a
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-01-04 22:50:13 -0600

    Update 059_ Functions.py

--- 01_Python_Tutorial/059_ Functions.py
+++ 01_Python_Tutorial/059_ Functions.py
@@ -118,3 +118,19 @@ def my_function(child3, child2, child1):
 
 my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
 
+# The phrase Keyword Arguments are often shortened to kwargs in Python documentations.
+
+# Arbitrary Keyword Arguments, **kwargs
+# If you do not know how many keyword arguments that will be passed into your function, add two asterisk: ** before the parameter name in the function definition.
+
+# This way the function will receive a dictionary of arguments, and can access the items accordingly:
+# ------------------------------------------------------------
+# Example
+# ------------------------------------------------------------
+# If the number of keyword arguments is unknown, add a double ** before the parameter name:
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+
