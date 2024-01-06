d2579d18729844b07c2d6b4d875ac84f696e8356
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-01-05 23:29:02 -0600

    Update 059_ Functions.py

--- 01_Python_Tutorial/059_ Functions.py
+++ 01_Python_Tutorial/059_ Functions.py
@@ -134,3 +134,35 @@ def my_function(**kid):
 
 my_function(fname = "Tobias", lname = "Refsnes")
 
+# Arbitrary Kword Arguments are often shortened to **kwargs in Python documentations.
+
+# Default Parameter Value
+# The following example shows how to use a default parameter value.
+
+# If we call the function without argument, it uses the default value:
+# ------------------------------------------------------------
+# Example
+# ------------------------------------------------------------
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+# Passing a List as an Argument
+# You can send any data types of argument to a function (string, number, list, dictionary etc.), and it will be treated as the same data type inside the function.
+
+# E.g. if you send a List as an argument, it will still be a List when it reaches the function:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
