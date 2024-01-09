21c77439fdad17cd0d9f7fef38ac98a31ea47605
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-01-08 22:54:51 -0600

    Update 059_ Functions.py

--- 01_Python_Tutorial/059_ Functions.py
+++ 01_Python_Tutorial/059_ Functions.py
@@ -185,3 +185,15 @@ print(my_function(9))
 # ---------------------------------------------------------------
 def myfunction():
   pass
+
+# Positional-Only Arguments
+# You can specify that a function can have ONLY positional arguments, or ONLY keyword arguments.
+
+# To specify that a function can have only positional arguments, add , / after the arguments:
+# ---------------------------------------------------------------
+# Example
+# ---------------------------------------------------------------
+def my_function(x, /):
+  print(x)
+
+my_function(3)
