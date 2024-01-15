a93281748b074be41b221ee52eb054fdbd1f1208
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-01-15 00:02:44 -0600

    Update 059_ Functions.py

--- 01_Python_Tutorial/059_ Functions.py
+++ 01_Python_Tutorial/059_ Functions.py
@@ -255,3 +255,29 @@ def my_function(a, b, /, *, c, d):
   print(a + b + c + d)
 
 my_function(5, 6, c = 7, d = 8)
+
+# Recursion
+# Python also accepts function recursion, which means a defined function can call itself.
+
+# Recursion is a common mathematical and programming concept. It means that a function calls itself. This has the benefit of meaning that you can loop through data to reach a result.
+
+# The developer should be very careful with recursion as it can be quite easy to slip into writing a function which never terminates, or one that uses excess amounts of memory or processor power. However, when written correctly recursion can be a very efficient and mathematically-elegant approach to programming.
+
+# In this example, tri_recursion() is a function that we have defined to call itself ("recurse"). We use the k variable as the data, which decrements (-1) every time we recurse. The recursion ends when the condition is not greater than 0 (i.e. when it is 0).
+
+# To a new developer it can take some time to work out how exactly this works, best way to find out is by testing and modifying it.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Recursion Example
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
