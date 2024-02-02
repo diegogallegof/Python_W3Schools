9c471871e4a0c3d1c0d6ea5de0ae6b13234027d0
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-02-01 23:45:36 -0600

    Update 060_Lambda.py

--- 01_Python_Tutorial/060_Lambda.py
+++ 01_Python_Tutorial/060_Lambda.py
@@ -51,3 +51,13 @@ mydoubler = myfunc(2)
 
 print(mydoubler(11))
 # ----------------------------------------------------------------
+
+# Or, use the same function definition to make a function that always triples the number you send in:
+
+# Example
+def myfunc(n):
+  return lambda a : a * n
+
+mytripler = myfunc(3)
+
+print(mytripler(11))
