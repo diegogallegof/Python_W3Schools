702726b6d6d346e7626046a24c9863caf3f1175c
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-02-02 23:01:47 -0600

    Update 060_Lambda.py

--- 01_Python_Tutorial/060_Lambda.py
+++ 01_Python_Tutorial/060_Lambda.py
@@ -53,11 +53,26 @@ print(mydoubler(11))
 # ----------------------------------------------------------------
 
 # Or, use the same function definition to make a function that always triples the number you send in:
-
+# ----------------------------------------------------------------
 # Example
+# ----------------------------------------------------------------
 def myfunc(n):
   return lambda a : a * n
 
 mytripler = myfunc(3)
 
 print(mytripler(11))
+
+# Or, use the same function definition to make both functions, in the same program:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
+Use lambda functions when an anonymous function is required for a short period of time.
