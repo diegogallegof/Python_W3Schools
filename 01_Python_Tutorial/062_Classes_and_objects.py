18de91a9a8d897786572b8b359a5dc2777366e73
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-02-17 23:32:06 -0600

    Update 062_Classes_and_objects.py

--- 01_Python_Tutorial/062_Classes_and_objects.py
+++ 01_Python_Tutorial/062_Classes_and_objects.py
@@ -13,7 +13,7 @@
 # Create a class named MyClass, with a property named x:
 
 class MyClass:
-  ddd
+  
   x = 5
 
 #   Create Object
@@ -24,3 +24,27 @@ class MyClass:
 
 p1 = MyClass()
 print(p1.x)
+
+# The __init__() Function
+# The examples above are classes and objects in their simplest form, and are not really useful in real life applications.
+
+# To understand the meaning of classes we have to understand the built-in __init__() function.
+
+# All classes have a function called __init__(), which is always executed when the class is being initiated.
+
+# Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
+
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Create a class named Person, use the __init__() function to assign values for name and age:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
