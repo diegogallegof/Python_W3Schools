6a3f9acd214e354304f7cfcddd38e8801a37fa63
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-02-18 23:53:08 -0600

    Update 062_Classes_and_objects.py

--- 01_Python_Tutorial/062_Classes_and_objects.py
+++ 01_Python_Tutorial/062_Classes_and_objects.py
@@ -48,3 +48,23 @@ p1 = Person("John", 36)
 
 print(p1.name)
 print(p1.age)
+
+# Note: The __init__() function is called automatically every time the class is being used to create a new object.
+
+# The __str__() Function
+# The __str__() function controls what should be returned when the class object is represented as a string.
+
+# If the __str__() function is not set, the string representation of the object is returned:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# The string representation of an object WITHOUT the __str__() function:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
