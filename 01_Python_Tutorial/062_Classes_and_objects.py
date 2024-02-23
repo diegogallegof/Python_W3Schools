1cb05e70661c255eea4cf935d53639bb653ad358
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-02-22 23:56:08 -0600

    Update 062_Classes_and_objects.py

--- 01_Python_Tutorial/062_Classes_and_objects.py
+++ 01_Python_Tutorial/062_Classes_and_objects.py
@@ -85,4 +85,22 @@ p1 = Person("John", 36)
 
 print(p1)
 
-# ----------------------------------------------------------------ç
+# Object Methods
+# Objects can also contain methods. Methods in objects are functions that belong to the object.
+
+# Let us create a method in the Person class:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Insert a function that prints a greeting, and execute it on the p1 object:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
