7565491d1a0643c5371da699f38185c9cb269b04
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-02-23 23:56:17 -0600

    Update 062_Classes_and_objects.py

--- 01_Python_Tutorial/062_Classes_and_objects.py
+++ 01_Python_Tutorial/062_Classes_and_objects.py
@@ -105,4 +105,16 @@ class Person:
 p1 = Person("John", 36)
 p1.myfunc()
 # ----------------------------------------
-# ----------------------------------------------------------------
+# ----------------------------------------------------------------s
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
