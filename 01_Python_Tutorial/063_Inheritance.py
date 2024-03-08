bd57e5a9c309dbe336818503faef93d39cb3fb41
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-03-07 23:54:38 -0600

    Update 063_Inheritance.py

--- 01_Python_Tutorial/063_Inheritance.py
+++ 01_Python_Tutorial/063_Inheritance.py
@@ -1,16 +1,17 @@
-Python Inheritance
-Python Inheritance
-Inheritance allows us to define a class that inherits all the methods and properties from another class.
+# Python Inheritance
 
-Parent class is the class being inherited from, also called base class.
+# Inheritance allows us to define a class that inherits all the methods and properties from another class.
 
-Child class is the class that inherits from another class, also called derived class.
+# Parent class is the class being inherited from, also called base class.
 
-Create a Parent Class
-Any class can be a parent class, so the syntax is the same as creating any other class:
+# Child class is the class that inherits from another class, also called derived class.
 
-ExampleGet your own Python Server
-Create a class named Person, with firstname and lastname properties, and a printname method:
+# Create a Parent Class
+# Any class can be a parent class, so the syntax is the same as creating any other class:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# Create a class named Person, with firstname and lastname properties, and a printname method:
 
 class Person:
   def __init__(self, fname, lname):
