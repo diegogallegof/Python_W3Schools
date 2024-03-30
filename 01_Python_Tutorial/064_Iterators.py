4596198f2d9842593d8fc28ab494e265853b9106
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-03-29 23:56:43 -0600

    Update 064_Iterators.py

--- 01_Python_Tutorial/064_Iterators.py
+++ 01_Python_Tutorial/064_Iterators.py
@@ -89,3 +89,32 @@ print(next(myiter))
 print(next(myiter))
 print(next(myiter))
 print(next(myiter))
+
+# StopIteration
+# The example above would continue forever if you had enough next() statements, or if it was used in a for loop.
+
+# To prevent the iteration from going on forever, we can use the StopIteration statement.
+
+# In the __next__() method, we can add a terminating condition to raise an error if the iteration is done a specified number of times:
+
+# Example
+# Stop after 20 iterations:
+
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
