90eff4197859799be909d4dda0f6596a2ff81348
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-04-02 23:55:05 -0600

    Update 065_Polymorphism.py

--- 01_Python_Tutorial/065_Polymorphism.py
+++ 01_Python_Tutorial/065_Polymorphism.py
@@ -22,3 +22,15 @@ print(len(x))
 mytuple = ("apple", "banana", "cherry")
 
 print(len(mytuple))
+
+#  Dictionary
+# For dictionaries len() returns the number of key/value pairs in the dictionary:
+
+# Example
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(len(thisdict))
