00085a32830ae04b756d4c95bb0023e25dbbabb2
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-04-04 23:24:04 -0600

    Update 065_Polymorphism.py

--- 01_Python_Tutorial/065_Polymorphism.py
+++ 01_Python_Tutorial/065_Polymorphism.py
@@ -76,3 +76,38 @@ for x in (car1, boat1, plane1):
   x.move()
 # Look at the for loop at the end. Because of polymorphism we can execute the same method for all three classes.
 
+# Inheritance Class Polymorphism
+# What about classes with child classes with the same name? Can we use polymorphism there?
+
+# Yes. If we use the example above and make a parent class called Vehicle, and make Car, Boat, Plane child classes of Vehicle, the child classes inherits the Vehicle methods, but can override them:
+
+# Example
+# Create a class called Vehicle and make Car, Boat, Plane child classes of Vehicle:
+
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Move!")
+
+class Car(Vehicle):
+  pass
+
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang") #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747") #Create a Plane object
+
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
