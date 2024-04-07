bddacc3ab0759f8ef62b8d7ac2c0ab2649847761
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-04-07 00:04:23 -0600

    Update 066_Scope.py

--- 01_Python_Tutorial/066_Scope.py
+++ 01_Python_Tutorial/066_Scope.py
@@ -12,3 +12,18 @@ def myfunc():
   print(x)
 
 myfunc()
+
+# Function Inside Function
+# As explained in the example above, the variable x is not available outside the function, but it is available for any function inside the function:
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# The local variable can be accessed from a function within the function:
+
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
+
+myfunc()
