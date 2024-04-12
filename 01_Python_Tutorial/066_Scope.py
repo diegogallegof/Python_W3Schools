58e32b6c775bbb556e7fada3165ce803a384dbb3
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-04-11 23:37:59 -0600

    Update 066_Scope.py

--- 01_Python_Tutorial/066_Scope.py
+++ 01_Python_Tutorial/066_Scope.py
@@ -79,3 +79,19 @@ def myfunc():
 myfunc()
 
 print(x)
+
+# Also, use the global keyword if you want to make a change to a global variable inside a function.
+# ----------------------------------------------------------------
+# Example
+# ----------------------------------------------------------------
+# To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+
+x = 300
+
+def myfunc():
+  global x
+  x = 200
+
+myfunc()
+
+print(x)
