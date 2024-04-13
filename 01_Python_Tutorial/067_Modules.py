f41e7fb7b2a2ddc06ebcf4c1a1e1dbc6e8ac7448
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-04-12 19:18:19 -0600

    Create 067_Modules.py

---
+++ 01_Python_Tutorial/067_Modules.py
@@ -0,0 +1,14 @@
+# Python Modules
+# What is a Module?
+# Consider a module to be the same as a code library.
+
+# A file containing a set of functions you want to include in your application.
+
+# Create a Module
+# To create a module just save the code you want in a file with the file extension .py:
+
+# Example
+# Save this code in a file named mymodule.py
+
+def greeting(name):
+  print("Hello, " + name)
