c903ad0d4074cf48c32534825e7e90fbf0356ba0
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-04-16 23:47:12 -0600

    Update 067_Modules.py

--- 01_Python_Tutorial/067_Modules.py
+++ 01_Python_Tutorial/067_Modules.py
@@ -38,3 +38,11 @@ person1 = {
   "age": 36,
   "country": "Norway"
 }
+
+# Example
+# Import the module named mymodule, and access the person1 dictionary:
+
+import mymodule
+
+a = mymodule.person1["age"]
+print(a)
