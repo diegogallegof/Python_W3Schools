ad70f326444eeb1c4999d004e4a7356037b85794
DonDiego <20131917+diegogallegof@users.noreply.github.com>
2024-04-17 23:56:24 -0600

    Update 067_Modules.py

--- 01_Python_Tutorial/067_Modules.py
+++ 01_Python_Tutorial/067_Modules.py
@@ -46,3 +46,17 @@ import mymodule
 
 a = mymodule.person1["age"]
 print(a)
+
+# Naming a Module
+# You can name the module file whatever you like, but it must have the file extension .py
+
+# Re-naming a Module
+# You can create an alias when you import a module, by using the as keyword:
+
+# Example
+# Create an alias for mymodule called mx:
+
+import mymodule as mx
+
+a = mx.person1["age"]
+print(a)
